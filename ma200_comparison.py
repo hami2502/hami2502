75da54ef17b0cc440b00f741db5cfd391f18dfcd
hami2502 <103120397+hami2502@users.noreply.github.com>
2024-01-26 23:01:04 +0100

    ma200_comparison.py aktualisieren

--- ma200_comparison.py
+++ ma200_comparison.py
@@ -1,29 +1,24 @@
 import yfinance as yf
 import sys
+import json
 
-# Aktiensymbol als Argument übernehmen
-ticker_symbol = sys.argv[1]
-
-# Daten von yfinance abrufen
-data = yf.download(ticker_symbol, period="1y")
-
-# Berechnen des 200-Tage-Durchschnitts
-data['MA200'] = data['Close'].rolling(window=200).mean()
-
-# Letzten verfügbaren Wert und MA200 abrufen
-latest_close = data['Close'].iloc[-1]
-latest_ma200 = data['MA200'].iloc[-1]
-
-# Vergleich und Ausgabe
-print(f"Aktueller Kurs von {ticker_symbol}: {latest_close}")
-print(f"MA200 von {ticker_symbol}: {latest_ma200}")
-
-percent_difference = ((latest_close - latest_ma200) / latest_ma200) * 100
-
-# Entscheidungslogik
-if percent_difference > 3:
-    sys.exit(1)
-elif percent_difference < -3:
-    sys.exit(-1)
-else:
-    sys.exit(0)
+# JSON-Datei einlesen
+with open('stocks.json', 'r') as file:
+    stocks = json.load(file)
+    
+for stock in stocks:
+    ticker_symbol = stock['ticker']
+    data = yf.download(ticker_symbol, period="1y")
+    data['MA200'] = data['Close'].rolling(window=200).mean()
+    latest_close = data['Close'].iloc[-1]
+    latest_ma200 = data['MA200'].iloc[-1]
+    print(f"Aktueller Kurs von {ticker_symbol}: {latest_close}")
+    print(f"MA200 von {ticker_symbol}: {latest_ma200}")
+    
+    percent_difference = ((latest_close - latest_ma200) / latest_ma200) * 100
+    if percent_difference > 3:
+        print(f"Kaufen")
+    elif percent_difference < -3:
+        print(f"Verkaufen")
+    else:
+        print(f"Halten")
