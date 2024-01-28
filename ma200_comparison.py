c1eae019b0bf1e13f0866c753b070418f393a44d
hami2502 <103120397+hami2502@users.noreply.github.com>
2024-01-28 12:15:42 +0100

    ma200_comparison.py aktualisieren

--- ma200_comparison.py
+++ ma200_comparison.py
@@ -2,6 +2,18 @@ import yfinance as yf
 import sys
 import json
 
+json
+
+# ATR berechnen
+def calculate_atr(data, window=14):
+    high_low = data['High'] - data['Low']
+    high_close = (data['High'] - data['Close'].shift()).abs()
+    low_close = (data['Low'] - data['Close'].shift()).abs()
+    ranges = pd.concat([high_low, high_close, low_close], axis=1)
+    true_range = ranges.max(axis=1)
+    atr = true_range.rolling(window=window).mean()
+    return atr
+
 # MACD und Signal Linie berechnen
 def calculate_macd(data):
     exp1 = data['Close'].ewm(span=12, adjust=False).mean()
@@ -18,17 +30,24 @@ for stock in stocks:
     ticker_symbol = stock['ticker']
     data = yf.download(ticker_symbol, period="1y")
     data['MA200'] = data['Close'].rolling(window=200).mean()
+    data['ATR'] = calculate_atr(data)
     data['MACD'], data['Signal'] = calculate_macd(data)
     latest_close = data['Close'].iloc[-1]
     latest_ma200 = data['MA200'].iloc[-1]
     latest_macd = data['MACD'].iloc[-1]
+    latest_atr = data['ATR'].iloc[-1]
     latest_signal = data['Signal'].iloc[-1]
 
+    # Stop-Loss und Take-Profit Levels festlegen
+    stop_loss = latest_close - latest_atr * 2
+    take_profit = latest_close + latest_atr * 2
+    
     print(f"Aktueller Kurs von {ticker_symbol}: {latest_close}")
     print(f"MA200 von {ticker_symbol}: {latest_ma200}")
     print(f"MACD von {ticker_symbol}: {latest_macd}")
     print(f"Signal von {ticker_symbol}: {latest_signal}")
-
+    print(f"{ticker_symbol}: ATR: {latest_atr}, Stop-Loss: {stop_loss}, Take-Profit: {take_profit}")
+    
     percent_difference = ((latest_close - latest_ma200) / latest_ma200) * 100
     if percent_difference > 3 or (latest_macd > latest_signal):
         print(f"Kaufen")
